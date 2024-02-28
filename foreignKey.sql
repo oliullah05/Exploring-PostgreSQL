a21db3bf41efb12dec2be329795bd52a47cef97b
oliullah05 <mdolihasan@gmail.com>
2024-02-28 21:35:49 +0600

    Enforcing Referential Integrity: Behaviors During Data Insertion

--- foreignKey.sql
+++ foreignKey.sql
@@ -9,3 +9,22 @@ create table "post" (
     title text not null,
     user_id int REFERENCES "user"(id)
 )
+
+insert into "user" (user_name) values 
+    ('akash'),
+   ('batash'),
+    ('sagor'),
+    ('nodi')
+
+INSERT INTO "post" (title, user_id) VALUES
+('Enjoying a sunny day with Akash!', 2),
+('Batash just shared an amazing recipe!', 1),
+('Exploring adventures with Sagor.', 4),
+('Nodi''s wisdom always leaves me inspired.', 4);
+
+select * from post
+-- add  constraint
+-- alter table post  alter column user_id set not null
+
+insert into post (title,user_id) values ('test',2)
+
