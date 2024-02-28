0153574d9cb32cacc8d16ddc332be6829533ed74
oliullah05 <mdolihasan@gmail.com>
2024-02-28 22:50:45 +0600

    Enforcing Referential Integrity: Behaviors During Data Deletion

--- foreignKey.sql
+++ foreignKey.sql
@@ -4,12 +4,11 @@ create table "user" (
     user_name VARCHAR(25) not null
 )
 
-create table "post" (
+CREATE TABLE "post" (
     id SERIAL PRIMARY KEY,
-    title text not null,
-    user_id int REFERENCES "user"(id)
-)
-
+    title TEXT NOT NULL,
+    user_id INT REFERENCES "user"(id) ON DELETE SET DEFAULT DEFAULT 2
+);
 insert into "user" (user_name) values 
     ('akash'),
    ('batash'),
@@ -22,9 +21,15 @@ INSERT INTO "post" (title, user_id) VALUES
 ('Exploring adventures with Sagor.', 4),
 ('Nodi''s wisdom always leaves me inspired.', 4);
 
-select * from post
+select * from "user";
+select * from post 
 -- add  constraint
 -- alter table post  alter column user_id set not null
 
-insert into post (title,user_id) values ('test',2)
+-- insert into post (title,user_id) values ('test',2)
+
+-- delete
+delete from "user" where id=4;
+drop table "user";
+drop table post;
 
